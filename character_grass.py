ee3f2f6fdc58316bb55fff05999db354660b888a
jsw1211 <jswtiger@naver.com>
2023-09-18 15:14:10 +0900

    사각형 무한루프

--- character_grass.py
+++ character_grass.py
@@ -1,7 +1,44 @@
 from pico2d import *
+import math
 
 open_canvas()
 
-# fill here
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+x = 400
+y = 90
+while(1):
+    while (x < 780):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = x + 2
+        delay(0.01)
+    while (y < 560):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y = y + 2
+        delay(0.01)
+    while (x > 20):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = x - 2
+        delay(0.01)
+    while (y > 90):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y = y - 2
+        delay(0.01)
+    while (x < 400):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = x + 2
+        delay(0.01)
+    
 
 close_canvas()
